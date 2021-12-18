1beeeda92658e49b75db68db5350f84846cb14c6
bozhko-victor <bozhko.v.n@yandex.ru>
2021-12-18 18:00:44 +0300

    added loginTest

--- src/test/java/tests/BrowserStackAndroidSelenideTests.java
+++ src/test/java/tests/BrowserStackAndroidSelenideTests.java
@@ -1,6 +1,8 @@
 package tests;
 
+import config.CredentialsConfig;
 import io.appium.java_client.MobileBy;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,8 @@ import static io.qameta.allure.Allure.step;
 @Tag("selenide_android")
 public class BrowserStackAndroidSelenideTests extends TestBase {
 
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
     @Test
     @DisplayName("Successful search")
     void searchTest() {
@@ -23,7 +27,6 @@ public class BrowserStackAndroidSelenideTests extends TestBase {
         step("Verify content found", () ->
                 $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container"))
                         .shouldHave(sizeGreaterThan(0)));
-
     }
 
     @Test
@@ -50,7 +53,36 @@ public class BrowserStackAndroidSelenideTests extends TestBase {
         });
 
         step("Check that the text has changed to Russian", () ->
-                $(MobileBy.id("org.wikipedia.alpha:id/view_page_title_text")).shouldHave(text("Джексон, Майкл")));
+                $(MobileBy.id("org.wikipedia.alpha:id/view_page_title_text")).shouldHave(text("Джексон, Майкл")
+        ));
+    }
+
+    @Test
+    @DisplayName("Successful login")
+    void logInTest() {
+        step("Open Menu", () ->
+            $(MobileBy.id("org.wikipedia.alpha:id/menu_overflow_button")).click());
+
+        step("Go to page 'Log in to Wikipedia'", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/explore_overflow_account_avatar")).click());
+
+        step("Set username", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/login_username_text")).setValue(config.login()));
+
+        step("Set password", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/login_password_input")).click();
+            $$(MobileBy.className("TextInputLayout")).get(1).
+                    $(MobileBy.className("android.widget.EditText")).setValue(config.password());
+        });
+
+        step("Click 'Log in'", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/login_button")).click());
+
+        step("Check that the username is displayed in the menu", () -> {
+            $(MobileBy.id("org.wikipedia.alpha:id/menu_overflow_button")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/explore_overflow_account_name")).
+                    shouldHave(text(config.login()));
+        });
     }
 
 }
